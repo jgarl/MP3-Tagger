9406717db13eb46063f0ad9e0b2acb9a4cdbc0e1
Juan <parrot@localhost.localdomain>
2022-12-07 11:20:07 +0100

    Remote file tagging

--- mp3Tagger.sh
+++ mp3Tagger.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-FILE_NAME=$1
+DIR=$(/bin/dirname "$1")
+BASE=$(/bin/basename "$1")
+EXT=$(echo "$BASE" | /usr/bin/awk -F. 'NF>1 {print $NF}')
 
 read -p "Song Title: " SONG_TITLE
 read -p "Artist Name: " ARTIST_NAME 
 read -p "Album Name: " ALBUM_NAME 
 read -p "Genre: " GENRE 
 
-eyeD3 -t "$SONG_TITLE" -a "$ARTIST_NAME" -A "$ALBUM_NAME" -G "$GENRE" "$FILE_NAME"; mv "$FILE_NAME" "$SONG_TITLE".mp3
+NAME="${SONG_TITLE}.${EXT}"
+FULL_NAME="${DIR}/${NAME}"
+
+eyeD3 -t "$SONG_TITLE" -a "$ARTIST_NAME" -A "$ALBUM_NAME" -G "$GENRE" "$1"; mv "$1" "$FULL_NAME"
